e605e9f6eb11bdee622cb265e5285bdab7ba3dcc
Rahul Gupta <rg5353070@gmail.com>
2024-08-04 13:50:41 +0530

    added scroll trigger animation to the set-up-avatar section

--- components/sections/home/HeroSection.tsx
+++ components/sections/home/HeroSection.tsx
@@ -1,15 +1,76 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { LucideAtom } from "lucide-react";
-import React from "react";
+import React, { useRef } from "react";
 import Placeholder from "@/public/placeholder.png";
 import Image from "next/image";
 import AvatarHeroGroup from "@/public/AvatarHeroGroup.png";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const HeroSection = () => {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      let t1 = gsap.timeline();
+
+      t1.fromTo(
+        ".main-content",
+        {
+          opacity: 0,
+          x: -100,
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 1,
+          ease: "power2.inOut",
+        }
+      );
+
+      t1.fromTo(
+        ".animate-image",
+        {
+          opacity: 0,
+          x: 100,
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 0.5,
+          ease: "power2.inOut",
+        }
+      );
+    },
+    {
+      scope: container,
+    }
+  );
+
+  // const renderLetters = (name: string, key: string) => {
+  //   if (!name) return;
+  //   return name.split("").map((letter, index) => (
+  //     <span
+  //       key={index}
+  //       className={`name-animation name-animation-${key}-index inline-block opacity-0 `}
+  //     >
+  //       {letter}
+  //     </span>
+  //   ));
+  // };
+
   return (
-    <section className="block-space-large bg-mainDark text-white">
-      <div className="container grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <div className="content-center space-y-4">
+    <section className="block-space-large bg-mainDark overflow-hidden text-white">
+      <div
+        className="container grid grid-cols-1 lg:grid-cols-2 gap-8"
+        ref={container}
+      >
+        <div className="content-center space-y-4 md:space-y-8 main-content">
           <span className="flex items-center gap-2">
             {" "}
             <LucideAtom className="text-blue text-4xl" />
@@ -48,7 +109,11 @@ const HeroSection = () => {
           </div>
         </div>
         <div>
-          <Image src={Placeholder} alt="" />
+          <Image
+            src={Placeholder}
+            alt="hero image of botbee illustrating use cases for the product itself"
+            className="animate-image"
+          />
         </div>
       </div>
     </section>
--- components/sections/home/SetUpAvatar.tsx
+++ components/sections/home/SetUpAvatar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import React from "react";
+import React, { useRef } from "react";
 import Placeholder from "@/public/placeholder.png";
 import Image from "next/image";
 import { TbWorld } from "react-icons/tb";
@@ -7,12 +9,47 @@ import { FaRegStar } from "react-icons/fa6";
 import { MdSecurity } from "react-icons/md";
 import { Separator } from "@/components/ui/separator";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { cn } from "@/lib/utils";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const SetUpAvatar = () => {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      let t1 = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top bottom",
+          end: "bottom top",
+          toggleActions: "restart pause resume pause",
+        },
+      });
+
+      t1.fromTo(
+        ".animate-content",
+        { opacity: 0, x: -100 },
+        { opacity: 1, x: 0, duration: 1, ease: "power2.inOut" }
+      );
+
+      t1.fromTo(
+        ".box",
+        { x: -100, opacity: 0 },
+        { x: 0, opacity: 1, duration: 0.5, stagger: 0.3, ease: "power1.inOut" }
+      );
+    },
+    { scope: container }
+  );
+
   return (
-    <section className="block-space bg-mainDark text-white">
-      <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
-          <div className="content-center space-y-4 md:space-y-6 lg:space-y-8">
+    <section className="block-space bg-mainDark text-white overflow-hidden">
+      <div className="container" ref={container}>
+        <div className="grid grid-cols-1 md:grid-cols-3  gap-4 md:gap-8">
+          <div className="content-center  space-y-4 md:space-y-6 animate-content lg:space-y-8">
             <h2>Set Up Your Avatar & Enjoy the Exclusive Features</h2>
             <p className="text-customMuted">
               Our platform is designed to scale effortlessly, allowing
@@ -26,7 +63,7 @@ const SetUpAvatar = () => {
               Build your Avatar
             </Button>
           </div>
-          <div className="col-span-2">
+          <div className="col-span-2 animate-content">
             <Image src={Placeholder} alt="" />
           </div>
         </div>
@@ -36,16 +73,16 @@ const SetUpAvatar = () => {
               icon={<TbWorld />}
               heading="50+ Language Support"
               tagline="Our Avatar team is equipped to assist you in over 50 languages, ensuring seamless communication no matter where you're from."
+              classname="border-r-2"
             />
-            <Separator orientation="vertical" className="hidden md:block" />
           </div>
           <div className="flex items-center">
             <IconHeadingContent
               icon={<FaRegStar />}
               heading="Low Cost Service"
               tagline="AI can handle numerous customer interactions simultaneously without additional costs and reducing the need for a large customer support team."
+              classname="border-r-2"
             />
-            <Separator orientation="vertical" className="hidden md:block" />
           </div>
           <IconHeadingContent
             icon={<MdSecurity />}
@@ -64,13 +101,15 @@ function IconHeadingContent({
   heading,
   tagline,
   icon,
+  classname,
 }: {
   heading: string;
   tagline: string;
   icon: any;
+  classname?: string;
 }) {
   return (
-    <div className="space-y-2">
+    <div className={cn("space-y-2 box", classname)}>
       <div className="text-blue text-4xl">{icon}</div>
       <h3>{heading}</h3>
       <span className="text-customMuted block">{tagline}</span>
